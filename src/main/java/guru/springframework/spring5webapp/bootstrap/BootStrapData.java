cc56c588feb3df1ddeb7ae75949051a02da61f09
Mateusz <mateuszkasprzyk01@gmail.com>
2022-06-08 19:37:28 +0200

    Added Publisher and relations

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -10,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
@@ -30,13 +31,16 @@ public class BootStrapData implements CommandLineRunner {
         System.out.println("BootStrapData running");
 
         Publisher publisher1 = new Publisher(
-                "Example Publishing", "King of the North st", "Wyzima", "KY", "43201");
+                "Example Publisher", "King of the North st", "Wyzima", "KY", "43201");
+        publisherRepository.save(publisher1);
 
         Author eldredge = new Author("John", "Eldredge");
         Author tolkien = new Author("J.R.R.", "Tolkien");
         Author lewis = new Author("C.S.", "Lewis");
         Author bartosiak = new Author("Jacek", "Bartosiak");
         Author zychowicz = new Author("Piotr", "Zychowicz");
+        List<Author> authors = Arrays.asList(eldredge, tolkien, lewis, bartosiak, zychowicz);
+        authorRepository.saveAll(authors);
 
         Book heart = new Book("Wild heart", "121212");
         Book fellowship = new Book("The Lord of the Rings: The Fellowship of the Ring", "131313");
@@ -44,6 +48,9 @@ public class BootStrapData implements CommandLineRunner {
         Book king = new Book("The Lord of the Rings: The Return of the King", "151515");
         Book divorce = new Book("The Great Divorce", "161616");
         Book wojna = new Book("Nadchodzi III Wojna Światowa", "171717");
+        List<Book> books = Arrays.asList(heart, fellowship, tower, king, divorce, wojna);
+        bookRepository.saveAll(books);
+
 
 
         eldredge.getBooks().add(heart);
@@ -53,6 +60,7 @@ public class BootStrapData implements CommandLineRunner {
         lewis.getBooks().add(divorce);
         bartosiak.getBooks().add(wojna);
         zychowicz.getBooks().add(wojna);
+        authorRepository.saveAll(authors);
 
         heart.getAuthors().add(eldredge);
         fellowship.getAuthors().add(tolkien);
@@ -61,14 +69,20 @@ public class BootStrapData implements CommandLineRunner {
         divorce.getAuthors().add(lewis);
         wojna.getAuthors().add(bartosiak);
         wojna.getAuthors().add(zychowicz);
+        tower.setPublisher(publisher1);
+        heart.setPublisher(publisher1);
+        bookRepository.saveAll(books);
 
+        publisher1.getBooks().add(tower);
+        publisher1.getBooks().add(heart);
         publisherRepository.save(publisher1);
-        authorRepository.saveAll(Arrays.asList(eldredge,tolkien,lewis,bartosiak,zychowicz));
-        bookRepository.saveAll(Arrays.asList(heart,fellowship,tower,king,divorce,wojna));
 
-        System.out.println(publisherRepository.count());
-        System.out.println(authorRepository.count());
-        System.out.println(bookRepository.count());
+        System.out.println("Publishers No. " + publisherRepository.count());
+        System.out.println("Author No. " + authorRepository.count());
+        authors.forEach(author -> System.out.println(author.getLastName() + ": " + author.getBooks().size() + " books"));
+        System.out.println("Books No. " + bookRepository.count());
+        books.forEach(book -> System.out.println(book.getTitle() + ": " + book.getAuthors().size() + " authors"));
+        System.out.println("Publisher's books: " + publisher1.getBooks().size());
 
     }
 }
